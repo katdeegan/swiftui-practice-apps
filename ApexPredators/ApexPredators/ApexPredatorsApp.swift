acf557c8917951fe29f5eb42ea614a0c6f2a5b8a
Katherine Deegan <katdeegan16@att.net>
2024-03-06 08:28:38 -0600

    first commit

---
+++ ApexPredators/ApexPredators/ApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorsApp.swift
+//  ApexPredators
+//
+//  Created by Katherine Deegan on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexPredators/ApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApexPredators
+//
+//  Created by Katherine Deegan on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
